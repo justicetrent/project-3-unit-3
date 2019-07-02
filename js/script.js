170f7aeeaa436556a81a9a0eeb70ef61c9cfbc38
Justice Trent <justice.trent@verizon.com>
2019-07-02 15:49:34 -0400

    Completed the Activity Section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $('#name').focus();                            // the cursor is focused on the "Name" textbox as soon as the page is loaded.
+
 $('#other-title').hide();                     //The intial hide of the 'other-title' textbox that I created 
 $('#title').on('change', function(){         //When the time comes for the user to chose a Job Role, the computer needs to perform this function. 
    if($(this).val() === 'other'){           // If the users job choice equal 'other'...
@@ -14,10 +15,52 @@ $('#design').on('change', function(){                                           
         <option value="gold">Gold (JS Puns shirt only)</option>`);
         $('#colors-js-puns').show();                                                                                            // ^above choses only the colors that are available with for the puns-js theme, using the backtick method.
     } else if ($('#design').val() === "heart js"){                                                                             // continues original if statement with a else if part that will select the remaining color options if the user decides to chose the other theme I heart js. 
-        $('#color').html(`<option value="tomato">Tomato (I &#9829; JS shirt only)</option>
+$('#color').html(`<option value="tomato">Tomato (I &#9829; JS shirt only)</option>
         <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option> 
         <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>`)
         $('#colors-js-puns').show();                                                                                        // Shows only the colors that coincide with the i heart js theme t-shirt. 
         
     } else { $('#colors-js-puns').hide();}                                                                                // Hides all of the colors when there is no t-shirt theme selected. 
 });
+
+let totalActivityCost = 0;
+let label = $(`<label>Total: $${totalActivityCost}</label>`);
+$('.activities').append(label);
+
+$('[type = "checkbox"]').change((event) => {
+     totalActivityCost = 0;
+    if($(`input[name='all']`).prop('checked')){
+        totalActivityCost += 200;
+    }
+    if($(`input[name='js-frameworks']`).prop('checked')){
+        $(`input[name='express']`).attr('disabled', true);
+        totalActivityCost += 100;
+    } else {$(`input[name='express']`).removeAttr('disabled');
+}
+    if($(`input[name='js-libs']`).prop('checked')){
+        $(`input[name='node']`).attr('disabled', true);
+        totalActivityCost += 100;
+    } else{ $(`input[name='node']`).removeAttr('disabled');
+}
+    if($(`input[name='node']`).prop('checked')){
+        $(`input[name='js-libs']`).attr('disabled', true);
+        totalActivityCost += 100;
+    } else{ $(`input[name='js-libs']`).removeAttr('disabled');
+}
+if($(`input[name='express']`).prop('checked')){
+    $(`input[name='js-frameworks']`).attr('disabled', true);
+    totalActivityCost += 100;
+} else{ $(`input[name='js-frameworks']`).removeAttr('disabled');
+}
+if($(`input[name='build-tools']`).prop('checked')){
+    totalActivityCost += 100;
+}
+if($(`input[name='npm']`).prop('checked')){
+    totalActivityCost += 100;
+}
+
+label.html(`<label>Total: $${totalActivityCost}</label>`);
+});
+
+
+
